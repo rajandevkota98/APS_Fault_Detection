d59fb31cd9ef453cc500ed00d9fa2afcc355b9fe
Rajan <r.devkota.98@gmail.com>
2023-01-06 09:57:16 +0545

    readme updated

---
+++ READ.md
@@ -0,0 +1,23 @@
+## APS Fault Detection
+
+* For Installation *
+```
+pip install -r requirements.txt
+
+```
+
+* Making connection to Mongodb *
+
+run
+
+```
+python data_dump.py
+
+```
+
+and now, run
+
+```
+python main.py
+
+```
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 
+# we create setup file to make our code to install it as an library. 
+# - e.means--> editable extention... here . is locating the present directory
 from setuptools import find_packages,setup
 
 from typing import List
@@ -10,6 +12,7 @@ def get_requirements()->List[str]:
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
+        print(requirement_list)
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
     
     if HYPHEN_E_DOT in requirement_list:
@@ -21,8 +24,10 @@ def get_requirements()->List[str]:
 setup(
     name="sensor",
     version="0.0.2",
-    author="ineuron",
-    author_email="avnish@ineuron.ai",
+    author="Rajan Devkota",
+    author_email="r.devkota.98@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
+
+
